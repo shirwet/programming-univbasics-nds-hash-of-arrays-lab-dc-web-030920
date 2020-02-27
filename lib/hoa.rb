dbee93cc22be3df4e8ab01d82a134e710117521d
shirwet Elfeel <eshirwet@yahoo.com>
2020-02-27 12:57:54 +0000

    Automatically backed up by Learn

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -1,11 +1,13 @@
+
+
+
+def add_character(show, name)
 BASE_HOA = {
   :chipmunks => ["Alvin", "Simon", "Theodore"],
   :third_earthers => ["Lion-O", "Cheetara", "Mumm-Ra (the ever-living)"],
   :jetsons => ["George", "Jane", "Judy", "Elroy"]
 }
 
-
-def add_character(show, name)
 
 return BASE_HOA[:chipmunks] << "Dave"
  return BASE_HOA[:jetsons] << "Astro"
